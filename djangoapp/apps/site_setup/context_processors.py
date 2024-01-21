5abc1820245f330da6272fd33f3485026898e0d1
Fellipe <anksrr@proton.me>
2024-01-20 21:45:01 -0300

    Created context_processors to site_setup in all templates

---
+++ djangoapp/apps/site_setup/context_processors.py
@@ -0,0 +1,7 @@
+from apps.site_setup.models import SiteSetup
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': setup,
+    }
--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.site_setup.context_processors.site_setup',
             ],
         },
     },
