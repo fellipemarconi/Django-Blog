a1d2310ae8b5d0acf6d55ebe15e07e7b9f84fa2f
Fellipe <anksrr@proton.me>
2024-01-22 20:42:06 -0300

    Created model Category to Blog with SlugField

--- djangoapp/apps/blog/admin.py
+++ djangoapp/apps/blog/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Tag
+from .models import Tag, Category
 
 # Register your models here.
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'name',
+    search_fields = 'id', 'name', 'slug',
+    list_per_page = 10
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
     list_display_links = 'name',
     search_fields = 'id', 'name', 'slug',
--- djangoapp/apps/blog/models.py
+++ djangoapp/apps/blog/models.py
@@ -17,3 +17,19 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = new_slugify(self.name, 4)
         return super().save(*args, **kwargs)
+    
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.name, 4)
+        return super().save(*args, **kwargs)
