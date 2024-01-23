23ae81a0285f671f50ef40342ec4320611f9aae9
Fellipe <anksrr@proton.me>
2024-01-22 21:54:25 -0300

    Optimized images to Post

--- djangoapp/apps/blog/models.py
+++ djangoapp/apps/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from utils.rands import new_slugify
 from django.contrib.auth.models import User
+from utils.images import resize_image
 
 # Create your models here.
 class Tag(models.Model):
@@ -115,4 +116,15 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = new_slugify(self.title, 4)
-        return super().save(*args, **kwargs)
+            
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+        
+        return super_save
