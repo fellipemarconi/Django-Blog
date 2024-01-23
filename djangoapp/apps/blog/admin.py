06c30f87b8d672fc0504b146b23dcac6e18cd479
Fellipe <anksrr@proton.me>
2024-01-22 21:50:26 -0300

    Created save_model to PostAdmin

--- djangoapp/apps/blog/admin.py
+++ djangoapp/apps/blog/admin.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib import admin
 from .models import Tag, Category, Page, Post
 
@@ -51,3 +52,11 @@ class PostAdmin(admin.ModelAdmin):
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+    
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+            
+        obj.save()
