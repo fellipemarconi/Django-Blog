a33d67f4a541e8beb6aae7a627001fcd8bb92e38
Fellipe <anksrr@proton.me>
2024-01-24 21:22:09 -0300

    Created a view to tags and put it in template of post

--- djangoapp/apps/blog/views.py
+++ djangoapp/apps/blog/views.py
@@ -73,3 +73,19 @@ def post(request, slug):
             'post': post,
         }
     )
+    
+def tag(request, slug):
+    posts = Post.objManager.get_published()\
+        .filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
