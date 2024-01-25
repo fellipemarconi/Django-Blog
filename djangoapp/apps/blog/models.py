7719d8c3318e144eb53d8e2b66c0d28b27039e7c
Fellipe <anksrr@proton.me>
2024-01-24 21:50:36 -0300

    Created view to filter the post

--- djangoapp/apps/blog/models.py
+++ djangoapp/apps/blog/models.py
@@ -60,6 +60,11 @@ class Page(models.Model):
     )
     content = models.TextField()
 
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:page', args=(self.slug,))
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = new_slugify(self.title, 4)
--- djangoapp/apps/blog/views.py
+++ djangoapp/apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from apps.blog.models import Post
+from apps.blog.models import Post, Page
 from django.db.models import Q
 
 PER_PAGE = 9
@@ -55,11 +55,18 @@ def category(request, slug):
     )
 
 def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+            'page': page,
         }
     )
 
